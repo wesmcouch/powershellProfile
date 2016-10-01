610c8d3fd204bbde4aa6f34b09ccefce21f73893
Wesley Couch <wcouch@purdue.edu>
2016-10-01 18:11:30 -0400

    Update loadProfile.ps1

--- loadProfile.ps1
+++ loadProfile.ps1
@@ -1,1 +1,11 @@
 #This script loads the powershell profile from github for a one liner in the console
+#If the powershell profile path is not created, create it
+$path = Split-Path -parent "$Profile"
+If(!(test-path "$Profile"))
+{
+    New-Item -Path $profile -ItemType "file" -Force
+}
+#Download the powershell profile
+cmd /c copy "$Profile" https://raw.githubusercontent.com/wesmcouch/powershellProfile/master/Microsoft.PowerShell_profile.ps1
+#Reload the profile into the current session
+.$Profile
