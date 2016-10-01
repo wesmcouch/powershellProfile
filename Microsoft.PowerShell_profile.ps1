7e3a110dc243c5f411274e64eddbaaf7a2cffc9a
Wesley Couch <wcouch@purdue.edu>
2016-10-01 17:57:27 -0400

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,29 @@
+Function psp {
+   param($InputObject = $null)
+
+   BEGIN {$status = $True}
+
+   PROCESS {
+      if ($InputObject -and $_) {
+         throw 'ParameterBinderStrings\AmbiguousParameterSet'
+      } elseif ($InputObject -or $_) {
+         $processObject = $(if ($InputObject) {$InputObject} else {$_})
+
+         write-host "Ping [$processObject]"
+
+         if( (Test-Connection $processObject -Quiet -count 1)) {
+            write-host "Ping response OK" -ForegroundColor DarkGreen
+         }
+         else {
+            write-host "Ping failed - host not found" -ForegroundColor red
+            $status = $False
+         }
+      }
+      else {throw 'ParameterBinderStrings\InputObjectNotBound'}
+
+    # next processObject
+    }
+
+    # Return True if pings to all machines succeed:
+    END {return $status}
+}
