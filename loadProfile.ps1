5d1bab9abec2384d8b75e6c6f38d45ffcacdce27
Wesley Couch <wcouch@purdue.edu>
2016-10-01 19:59:56 -0400

    Update loadProfile.ps1

--- loadProfile.ps1
+++ loadProfile.ps1
@@ -5,7 +5,7 @@ If(!(test-path "$Profile"))
     $path1 = Split-Path -Path "$Profile"
     New-Item -ItemType Directory -Force -Path $path1
     $WebClient2 = New-Object System.Net.WebClient
-    $WebClient2.DownloadFile('https://raw.githubusercontent.com/wesmcouch/powershellProfile/master/Microsoft.PowerShell_profile.ps1',"$Profile")
+    $WebClient2.DownloadFile('https://raw.githubusercontent.com/wesmcouch/powershellProfile/master/Microsoft.PowerShell_profile.ps1','"$Profile"')
 }
 #Reload the profile into the current session
 .$Profile
