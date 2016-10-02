32ca1bfb05735e838823e00d8d9a0f724892bbe4
Wesley Couch <wcouch@purdue.edu>
2016-10-01 20:19:28 -0400

    Update installSSH.ps1

--- installSSH.ps1
+++ installSSH.ps1
@@ -1,3 +1,2 @@
-$outzip = "C:\openssh.zip"
-Invoke-WebRequest -Uri "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v0.0.1.0/OpenSSH-Win64.zip" -OutFile $outzip
-Unzip $outzip "C:\openssh"
+Invoke-WebRequest -Uri "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v0.0.1.0/OpenSSH-Win64.zip" -OutFile "C:\openssh.zip"
+Unzip "C:\openssh.zip" "C:\openssh"
